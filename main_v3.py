a4e09373c07caa36f2b1378e095e3604033a04b9
bdehapiot <b.dehapiot@gmail.com>
2022-12-04 21:37:42 +0100

    upadate

--- main_v3.py
+++ main_v3.py
@@ -13,8 +13,8 @@ from tools.idx import rwhere
 
 #%% Get raw name
 
-# stack_name = 'C1-2022.07.05_Luminy_22hrsAPF_Phallo568_aAct488_405nano2_100Xz2.5_AS_488LP4_1_1.tif'
-stack_name = 'C1-2022.07.05_Luminy_24hrsAPF_Phallo568_aAct488_405nano2_100Xz2.5_AS_488LP4_1_1.tif'
+stack_name = 'C1-2022.07.05_Luminy_22hrsAPF_Phallo568_aAct488_405nano2_100Xz2.5_AS_488LP4_1_1.tif'
+# stack_name = 'C1-2022.07.05_Luminy_24hrsAPF_Phallo568_aAct488_405nano2_100Xz2.5_AS_488LP4_1_1.tif'
 # stack_name = 'C1-2022.07.05_Luminy_26hrsAPF_Phallo568_aAct488_405nano2_100Xz2.5_AS_488LP4_2_2.tif'
 # stack_name = 'C1-2022.07.05_Luminy_28hrsAPF_Phallo568_aAct488_405nano2_100Xz2.5_AS_1_1.tif'
 # stack_name = 'C1-2022.07.05_Luminy_30hrsAPF_Phallo568_aAct488_405nano2_100Xz2.5_AS_1_1.tif'
@@ -34,26 +34,24 @@ thresh_coeff = 0.75
 
 # Gabor kernels
 n_kernels = 32
-kernel_size = patch_size
-sigma = 4
-lmbda = 8
-gamma = 0.5
-psi = 0
+sigma = 3
 
 #%%
 
 img = stack[9,...] # extract img
 
-
 # -----------------------------------------------------------------------------
 
 from skimage.filters import threshold_li
+from skimage.morphology import remove_small_holes, remove_small_objects
 
 start = time.time()
 print('Get mask')
 
 thresh = threshold_li(img, tolerance=1)
 mask = img > thresh*thresh_coeff
+mask = remove_small_holes(mask, area_threshold=512)
+mask = remove_small_objects(mask, min_size=512)
 
 end = time.time()
 print(f'  {(end-start):5.3f} s') 
@@ -64,36 +62,21 @@ from skimage.filters import scharr_h, scharr_v, rank
 from skimage.morphology import disk
 
 start = time.time()
-print('Get local angles')
+print('Get local orientation')
 
-# Get local angles (using scharr filter)
-scharr_h = scharr_h(img); scharr_v = scharr_v(img)
-angles = np.arctan2(scharr_h, scharr_v) * (180/np.pi) % 180
-angles = rank.median(
-    angles.astype('uint8'), 
-    footprint=disk(patch_size),
-    mask=mask,
-    ).astype('float')
-angles[mask==0] = np.nan
-
-end = time.time()
-print(f'  {(end-start):5.3f} s')  
-
-# -----------------------------------------------------------------------------
-
-start = time.time()
-print('Define gabor filters')
-
-# Define gabor filters
-kernels = []    
-thetas = np.arange(0, np.pi, np.pi/n_kernels)
-for theta in thetas:        
-    kernel = cv2.getGaborKernel(
-        (kernel_size, kernel_size), 
-        sigma, theta, lmbda, gamma, psi, 
-        ktype=cv2.CV_64F
-        )
-    kernels.append(kernel)
+def local_gradient(img, mask): 
+    
+    gradient_h = scharr_h(img); gradient_v = scharr_v(img)
+    magnitude = np.sqrt((gradient_h ** 2) + (gradient_v ** 2))
+    orientation = np.arctan2(gradient_h, gradient_v) * (180/np.pi) % 180
+    orientation = rank.median(
+        orientation.astype('uint8'), 
+        footprint=disk(patch_size*2),
+        mask=mask,
+        ).astype('float')
+    orientation[mask==0] = np.nan
+    
+    return magnitude, orientation
 
 end = time.time()
 print(f'  {(end-start):5.3f} s')  
@@ -103,39 +86,89 @@ print(f'  {(end-start):5.3f} s')
 start = time.time()
 print('Apply gabor filters')
 
-def apply_gfilt(img, kernels, thetas, ):
-   
-    # Apply gabor filters
-    gfilt = np.zeros((n_kernels, img.shape[0], img.shape[1]))
-    for k, kernel in enumerate(kernels):
-        gfilt[k,...] = cv2.filter2D(img, -1, kernel)
-        
-    # Find best angle    
-    best_angle = np.zeros((len(thetas), angles.shape[0], angles.shape[1]))
-    for i in range(len(thetas)):
-        best_angle[i,...] = np.abs(angles - thetas[i]*180/np.pi) 
-    argmin = np.argmin(best_angle, axis=0)
+def apply_gabor_filters(img, orientation, n_kernels=32, sigma=4):
+           
+    # Get gabor parameters
+    kernel_size = sigma*10
+    lmbda = sigma*3
+    gamma = 0.5
+    psi = 0
     
-    # Project best angle 
+    # Get gabor filters
+    kernels = [] 
+    thetas = np.arange(0, np.pi, np.pi/n_kernels)
+    for theta in thetas:        
+        kernel = cv2.getGaborKernel(
+            (kernel_size, kernel_size), 
+            sigma, theta, lmbda, gamma, psi, 
+            ktype=cv2.CV_64F
+            )
+        kernels.append(kernel)
+    
+    # Apply all gabor filters
+    all_filt = np.zeros((n_kernels, img.shape[0], img.shape[1]))
+    for k, kernel in enumerate(kernels):
+        all_filt[k,...] = cv2.filter2D(img, -1, kernel)
+        
+    # Find best orientation 
+    thetas = np.arange(0, np.pi, np.pi/len(kernels)) 
+    best_orientation = np.zeros((len(thetas), orientation.shape[0], orientation.shape[1]))
+    for i in range(len(thetas)):
+        best_orientation[i,...] = np.abs(orientation - thetas[i]*180/np.pi) 
+    argmin = np.argmin(best_orientation, axis=0)
+    
+    # Project best orientation 
     idx = rwhere(mask, 1)
     idx = tuple(argmin[idx]) + idx 
-    proj = np.zeros_like(gfilt)
-    proj[idx] = gfilt[idx]
-    proj = np.max(proj, axis=0)
+    gfilt = np.zeros_like(all_filt)
+    gfilt[idx] = all_filt[idx]
+    gfilt = np.max(gfilt, axis=0)
     
-    return 
-    
+    return gfilt
+
+end = time.time()
+print(f'  {(end-start):5.3f} s')  
+
+# -----------------------------------------------------------------------------
+
+magnitude, orientation = local_gradient(img, mask)
+gfilt1 = apply_gabor_filters(img, orientation, n_kernels=n_kernels, sigma=sigma)
+
+# magnitude, orientation = local_gradient(gfilt1, mask)
+gfilt2 = apply_gabor_filters(gfilt1, orientation, n_kernels=n_kernels, sigma=sigma+1)
+
+# magnitude, orientation = local_gradient(gfilt2, mask)
+gfilt3 = apply_gabor_filters(gfilt2, orientation, n_kernels=n_kernels, sigma=sigma+2)
+
+# -----------------------------------------------------------------------------
+
+from skimage.morphology import skeletonize
+
+start = time.time()
+print('??? #2')
+
+gthresh = threshold_li(gfilt3, tolerance=1)
+gmask = gfilt3 > gthresh*3 # adjust !!!
+gmask = remove_small_holes(gmask, area_threshold=128)
+gmask = remove_small_objects(gmask, min_size=128)
+gskel = skeletonize(gmask)
+
 end = time.time()
 print(f'  {(end-start):5.3f} s')  
 
 # -----------------------------------------------------------------------------
 
 viewer = napari.Viewer()
-viewer.add_image(img)
-# viewer.add_image(gfilt)
-viewer.add_image(angles)
-# viewer.add_image(best_angle)
-viewer.add_image(proj)
+viewer.add_image(img, contrast_limits=(0, 7500))
+# viewer.add_image(mask)
+# viewer.add_image(magnitude)
+# viewer.add_image(orientation)
+# viewer.add_image(np.array(kernels))
+# viewer.add_image(gfilt1) 
+# viewer.add_image(gfilt2)
+viewer.add_image(gfilt3)
+# viewer.add_image(gmask)
+# viewer.add_image(gskel, blending='additive')
 
 #%%
 
@@ -148,15 +181,15 @@ viewer.add_image(proj)
 # ny = img.shape[0]//patch_size
 # nx = img.shape[1]//patch_size
 
-# patch_angles = np.reshape(
-#     patchify(angles, patch_size, patch_size), 
+# patch_orientation = np.reshape(
+#     patchify(orientation, patch_size, patch_size), 
 #     (ny*nx, patch_size, patch_size)
 #     )
 
-# angles_med = np.nanmedian(patch_angles, axis=(1,2))
-# angles_med = np.reshape(angles_med, (ny,nx))
-# angles_med = nanfilt(angles_med, 5, 'median')
-# angles_med = resize(angles_med, (img.shape), order=0)
+# orientation_med = np.nanmedian(patch_orientation, axis=(1,2))
+# orientation_med = np.reshape(orientation_med, (ny,nx))
+# orientation_med = nanfilt(orientation_med, 5, 'median')
+# orientation_med = resize(orientation_med, (img.shape), order=0)
 
 # end = time.time()
 # print(f'  {(end-start):5.3f} s')  
