ebe32d3bd9e2fdc0460af73556878a277f153cc1
bdehapiot <b.dehapiot@gmail.com>
2022-12-12 10:17:58 +0100

    update

--- main.py
+++ main.py
@@ -6,337 +6,252 @@ import napari
 import numpy as np
 from skimage import io 
 from pathlib import Path
+from aicsimageio.readers import CziReader
 from joblib import Parallel, delayed
-from skimage.transform import resize
-from skimage.filters import threshold_li
-from skimage.morphology import skeletonize
 
-from tools.nan import nanfilt
+from dtype import ranged_conversion
+from nan import nanfilt, nanreplace
+from skel import pixconn
 
-#%% Get raw name
+#%% Hyperstack name
 
-# stack_name = 'C1-2022.07.05_Luminy_22hrsAPF_Phallo568_aAct488_405nano2_100Xz2.5_AS_488LP4_1_1.tif'
-stack_name = 'C1-2022.07.05_Luminy_24hrsAPF_Phallo568_aAct488_405nano2_100Xz2.5_AS_488LP4_1_1.tif'
-# stack_name = 'C1-2022.07.05_Luminy_26hrsAPF_Phallo568_aAct488_405nano2_100Xz2.5_AS_488LP4_2_2.tif'
-
-#%% Get path and open data
-
-data_path = Path(Path.cwd(), 'data')
-stack_path = Path(data_path, stack_name)
-stack = io.imread(stack_path)
-
-stack = stack[9,...]
+hstack_name = '2022.10.31_22hrsAPF_Luminy_phallo568_405nano2_488_nano42_647nano62_100Xz2.5_1_1.czi'
+# hstack_name = '2022.10.31_24hrsAPF_Luminy_phallo568_405nano2_488_nano42_647nano62_100Xz2.5_1_1.czi'
+# hstack_name = '2022.10.31_26hrsAPF_Luminy_phallo568_405nano2_488_nano42_647nano62_100Xz2.5_1_1.czi'
+# hstack_name = '2022.10.31_28hrsAPF_Luminy_phallo568_405nano2_488_nano42_647nano62_100Xz2.5_1_1.czi'
+# hstack_name = '2022.10.31_30hrsAPF_Luminy_phallo568_405nano2_488_nano42_647nano62_100Xz2.5_1_1.czi'
+# hstack_name = '2022.10.31_32hrsAPF_Luminy_phallo568_405nano2_488_nano42_647nano62_100Xz2.5_1_1.czi'
+# hstack_name = '2022.10.31_48hrsAPF_Luminy_phallo568_405nano2_488_nano42_647nano62_100Xz2.5_1_1.czi'
 
 #%% Parameters
 
-# Gabor filtering
-n_filters=16
-kernel_size=100
-sigma=4
-lmbda=10
-gamma=0.25
-psi=0
-parallel=False
+# Get orientations
+ridge_channel = 1
+thresh_coeff = 0.75
+rsize_factor = 0.5
+min_magnitude = 0.05
+smooth_size = 32
 
-# Patch
-patch_size = 20
-thresh_coeff = 2
+# Get gabor
+n_kernels = 16
+sigma = 3
 
-#%% Functions: gabor_filtering
+#%% Open czi file
 
-def gabor_filtering(stack, n_filters, kernel_size, sigma, lmbda, gamma, psi, parallel=False):
-    
-    # Nested functions --------------------------------------------------------
-    
-    def _gabor_filtering(img):
-        
-        # Apply filters
-        img_filt = np.zeros((n_filters, img.shape[0], img.shape[1]))
-        for i, kernel in enumerate(filters):
-            img_filt[i,...] = cv2.filter2D(img, -1, kernel)  
+hstack_path = Path(Path(Path.cwd(), 'data'), hstack_name)
+hstack_czi = CziReader(hstack_path)
+hstack = hstack_czi.data.squeeze()
 
-        return img_filt
+# Get voxel size (µm)
+vox_size = (
+    hstack_czi.physical_pixel_sizes.Y,
+    hstack_czi.physical_pixel_sizes.X,
+    hstack_czi.physical_pixel_sizes.Z
+    )
+
+#%%
+
+# Extract image for running tests
+zstack = hstack[ridge_channel,...]
+img = zstack[zstack.shape[0]//2,...]
+
+#%%
+
+from skimage.morphology import disk
+from skimage.transform import rescale
+from skimage.morphology import binary_dilation
+from skimage.morphology import remove_small_holes, remove_small_objects
+from skimage.filters import threshold_li, scharr_h, scharr_v, gaussian, rank
+
+start = time.time()
+print('get_orientations')
+
+# Initialize
+smooth_size = int(smooth_size*rsize_factor)
+
+# Resize & process img
+rsize = rescale(img, rsize_factor, preserve_range=True)
+rsize = ranged_conversion(
+    rsize, intensity_range=(5,95), spread=3, dtype='float')
+
+# Get mask
+mask = rsize > threshold_li(rsize, tolerance=1)*thresh_coeff
+mask = remove_small_holes(mask, area_threshold=rsize.shape[0])
+mask = remove_small_objects(mask, min_size=rsize.shape[0])
+
+# Extract local gradient
+gradient_h = scharr_h(rsize); gradient_v = scharr_v(rsize)
+magnitudes = np.sqrt((gradient_h ** 2) + (gradient_v ** 2))
+orientations = np.arctan2(gradient_h, gradient_v) * (180/np.pi) % 180
+
+# Remove low magnitude orientations
+orientations[magnitudes < min_magnitude] = np.nan
+orientations[mask == False] = np.nan
+orientations = nanreplace(
+    orientations, kernel_size=smooth_size, method='median', mask=mask)
+
+# Smooth orientations map   
+orientations = rank.median(
+    orientations.astype('uint8'), 
+    footprint=disk(smooth_size),
+    mask=mask,
+    ).astype('float')
+orientations[mask == False] = np.nan
+
+# Rescale and remove out of mask signal
+magnitudes = rescale(magnitudes, 1/rsize_factor, preserve_range=True)
+orientations = rescale(orientations, 1/rsize_factor, preserve_range=True)
+
+end = time.time()
+print(f'  {(end-start):5.3f} s')  
+
+#%%
+
+def get_gabor(img, orientations, n_kernels=16, sigma=3):
+           
+    # Set gabor parameters (according to sigma)
+    kernel_size = sigma*10
+    lmbda = sigma*3
+    gamma = 0.5
+    psi = 0
     
-    # Run ---------------------------------------------------------------------
-    
-    # Create filters  
-    filters = []    
-    thetas = np.arange(0, np.pi, np.pi/n_filters)
+    # Design gabor filters
+    kernels = [] 
+    thetas = np.arange(0, np.pi, np.pi/n_kernels)
     for theta in thetas:        
         kernel = cv2.getGaborKernel(
             (kernel_size, kernel_size), 
             sigma, theta, lmbda, gamma, psi, 
             ktype=cv2.CV_64F
             )
-        kernel /= 1.0 * kernel.sum() # Brightness normalization
-        filters.append(kernel)
-        
-    # Add one dimension (if ndim == 2)
-    ndim = (stack.ndim)        
-    if ndim == 2:
-        stack = stack.reshape((1, stack.shape[0], stack.shape[1]))  
-            
-    if parallel:
-
-        # Run parallel
-        output_list = Parallel(n_jobs=-1)(
-            delayed(_gabor_filtering)(
-                img,
-                )
-            for img in stack
-            )
-        
-    else:
-        
-        # Run serial
-        output_list = [_gabor_filtering(
-                img,
-                )
-            for img in stack
-            ]
+        kernels.append(kernel)
     
-    # Extract outputs
-    stack_filt = np.stack([data for data in output_list], axis=0).squeeze()
+    # Apply all gabor filters
+    all_filters = np.zeros((n_kernels, img.shape[0], img.shape[1]))
+    for k, kernel in enumerate(kernels):
+        all_filters[k,...] = cv2.filter2D(img, -1, kernel)
+        
+    # Find best filter (according to orientations)
+    best_filter = np.zeros((
+        len(thetas), 
+        orientations.shape[0],
+        orientations.shape[1]
+        ))
+    for i in range(len(thetas)):
+        best_filter[i,...] = np.abs(orientations - thetas[i]*180/np.pi) 
+    best_filter = np.argmin(best_filter, axis=0)
     
-    return stack_filt
-
-#%% Functions: patch
-
-def patch_analysis(stack, stack_filt, patch_size, thresh_coeff):
+    # Project best filter only
+    mask = ~np.isnan(orientations)
+    idx = np.where(mask == True)
+    idx = (best_filter[idx], idx[0], idx[1]) 
+    gabor = np.zeros_like(all_filters)
+    gabor[idx] = all_filters[idx]
+    gabor = np.max(gabor, axis=0)
     
-    # Nested functions --------------------------------------------------------
-    
-    def _patch_analysis(img, img_filt):
-        
-        local_mean = np.zeros((img.shape[0]//patch_size, img.shape[1]//patch_size))
-        local_argmax = np.zeros((img.shape[0]//patch_size, img.shape[1]//patch_size))                 
-        
-        for y, yi in enumerate(np.arange(0, img.shape[0], patch_size)):
-            for x, xi in enumerate(np.arange(0, img.shape[1], patch_size)):
-                
-                # Get local mean (img)
-                patch = img[yi:yi+patch_size,xi:xi+patch_size]  
-                local_mean[y,x] = np.mean(patch)
-                     
-                # Get local angle (img_filt)
-                patch_filt = img_filt[:,yi:yi+patch_size,xi:xi+patch_size]        
-                local_argmax[y,x] = np.argmax(np.std(patch_filt, axis=(1,2)))
-                
-        # Process local angle         
-        thresh = threshold_li(stack) * thresh_coeff
-        local_argmax[local_mean<thresh] = np.nan
-        local_argmax = nanfilt(local_argmax.astype('float'), 3, 'median')
-        
-        # Get vectors
-        
-                             
-        return local_mean, local_argmax
-    
-    # Run ---------------------------------------------------------------------
-
-    # Add one dimension (if ndim == 2)
-    ndim = (stack.ndim)        
-    if ndim == 2:
-        stack = stack.reshape((1, stack.shape[0], stack.shape[1]))  
-        stack_filt = stack_filt.reshape((1, stack_filt.shape[0], stack_filt.shape[1])) 
-
-    if parallel:
-
-        # Run parallel
-        output_list = Parallel(n_jobs=-1)(
-            delayed(_patch_analysis)(
-                img,
-                img_filt,
-                )
-            for img, img_filt in zip(stack, stack_filt)
-            )
-        
-    else:
-        
-        # Run serial
-        output_list = [_patch_analysis(
-                img,
-                img_filt,
-                )
-            for img, img_filt in zip(stack, stack_filt)
-            ]
-        
-    # Extract outputs
-    local_mean = np.stack([data[0] for data in output_list], axis=0).squeeze()    
-    local_argmax = np.stack([data[1] for data in output_list], axis=0).squeeze()     
-    
-    return local_mean, local_argmax
-        
-#%%
+    return gabor
 
 start = time.time()
-print('gabor_filtering')
-    
-stack_filt = gabor_filtering(
-    stack,
-    n_filters,
-    kernel_size,
-    sigma,
-    lmbda,
-    gamma,
-    psi,
-    parallel
-    )
+print('get_gabor')
+
+gabor = get_gabor(img, orientations,
+    n_kernels=n_kernels, sigma=sigma)
+gabor = get_gabor(gabor, orientations,
+    n_kernels=n_kernels, sigma=sigma+1)
+gabor = get_gabor(gabor, orientations,
+    n_kernels=n_kernels, sigma=sigma+2)
+
+end = time.time()
+print(f'  {(end-start):5.3f} s') 
+
+#%%
+
+from skimage.morphology import skeletonize, label
+
+start = time.time()
+print('get skeleton')
+
+gthresh = threshold_li(gabor, tolerance=1)
+gmask = gabor > gthresh*3 # adjust !!!
+gmask = remove_small_holes(gmask, area_threshold=128)
+gmask = remove_small_objects(gmask, min_size=128)
+skel = skeletonize(gmask)
+skel = skel ^ (pixconn(skel, conn=2) > 2)
+skel = remove_small_objects(skel, min_size=32, connectivity=2)
+labels = label(skel, connectivity=2)
 
 end = time.time()
 print(f'  {(end-start):5.3f} s')  
 
 # -----------------------------------------------------------------------------
 
-# start = time.time()
-# print('patch_analysis')
-    
-# local_mean, local_argmax = patch_analysis(
-#     stack,
-#     stack_filt,
-#     patch_size,
-#     thresh_coeff,
-#     )
+start = time.time()
+print('Measure along skeleton')
 
-# end = time.time()
-# print(f'  {(end-start):5.3f} s')  
+# Initialize
+connect_kernel = np.array(
+    [[1, 2, 3],
+     [8, 0, 4],
+     [7, 6, 5]])
 
-# -----------------------------------------------------------------------------
+y_shift = [-1, -1, -1, 0, 1, 1, 1, 0]
+x_shift = [-1, 0, 1, 1, 1, 0, -1, -1]
 
-# viewer = napari.Viewer()
-# viewer.add_image(local_mean)
-# viewer.add_image(local_argmax)    
+# Pad img & labels
+img = np.pad(img, 
+    pad_width=((1, 1), (1, 1)),
+    mode='constant', constant_values=0)   
+labels = np.pad(labels, 
+    pad_width=((1, 1), (1, 1)),
+    mode='constant', constant_values=0)   
+
+# Extract seeds (top left pixel for every ridge) 
+y,x = np.where(pixconn(labels>0, conn=2)==1)
+l = labels[y,x]; endpoints = np.column_stack((l,y,x))
+idx = np.lexsort((endpoints[:,2],endpoints[:,1],endpoints[:,0]))  
+seeds = np.take(endpoints[idx], np.arange(0, len(endpoints), 2), axis=0)
+
+ridge_data = []
+for i in range(np.max(labels)):
+        
+    # Isolate ridge and coordinates
+    ridge = (labels==i+1).astype('uint8')
+    y = seeds[i,1]; x = seeds[i,2]
+
+    # Ridge intensity/distance profiles
+    distance = np.zeros((np.sum(ridge>0)))
+    intensity = np.zeros((np.sum(ridge>0)))
+    for j in range(np.sum(ridge>0)):
+        
+        intensity[j] = img[y,x]
+        
+        connect = np.sum(ridge[y-1:y+2,x-1:x+2]*connect_kernel)        
+        if j == 0:          
+            distance[j] = 0            
+        else:            
+            if connect % 2 == 0:              
+                distance[j] = distance[j-1] + 1            
+            else:                
+                distance[j] = distance[j-1] + np.sqrt(2)
+        
+        ridge[y,x] = 0
+        y = y + y_shift[connect-1] 
+        x = x + x_shift[connect-1]   
+        
+    # Append ridge data
+    ridge_data.append((distance, intensity))
+
+end = time.time()
+print(f'  {(end-start):5.3f} s')  
 
 #%%
-
-thresh_coeff = 0.75
-thresh = threshold_li(stack) * thresh_coeff
-
-# -----------------------------------------------------------------------------
-
-thetas = np.arange(0, np.pi, np.pi/n_filters)
-img = stack
-img_filt = stack_filt
-
-start = time.time()
-print('patch_analysis')
-          
-local_mean = np.zeros((img.shape[0]//patch_size, img.shape[1]//patch_size))
-local_argmax = np.zeros((img.shape[0]//patch_size, img.shape[1]//patch_size))                 
-for y, yi in enumerate(np.arange(0, img.shape[0], patch_size)):
-    for x, xi in enumerate(np.arange(0, img.shape[1], patch_size)):
-        
-        # Get local mean (img)
-        patch = img[yi:yi+patch_size,xi:xi+patch_size]  
-        local_mean[y,x] = np.mean(patch)
-             
-        # Get max sd (img_filt)
-        patch_filt = img_filt[:,yi:yi+patch_size,xi:xi+patch_size]        
-        local_argmax[y,x] = np.argmax(np.std(patch_filt, axis=(1,2)))
-        
-# Process local angle         
-thresh = threshold_li(stack) * thresh_coeff
-local_argmax[local_mean<thresh] = np.nan
-local_argmax = nanfilt(local_argmax.astype('float'), 5, 'mean')
-
-# Get vectors 
-vectors = []
-for y, yi in enumerate(np.arange(0, img.shape[0], patch_size)):
-    for x, xi in enumerate(np.arange(0, img.shape[1], patch_size)):
-        
-        if local_mean[y,x] > thresh:
-            vectors.append(np.array([[yi, xi], [
-                np.sin(thetas[int(local_argmax[y,x])]-np.pi/2),
-                np.cos(thetas[int(local_argmax[y,x])]-np.pi/2)            
-                ]]))
-        else:
-            vectors.append(np.array([[yi, xi], [0,0]]))      
-
-end = time.time()
-print(f'  {(end-start):5.3f} s')
-
-# -----------------------------------------------------------------------------
-
-start = time.time()
-print('??? Proj')
-        
-proj_filt = np.zeros((img.shape[0], img.shape[1]))
-for y, yi in enumerate(np.arange(0, img.shape[0], patch_size)):
-    for x, xi in enumerate(np.arange(0, img.shape[1], patch_size)):
-        
-        if not np.isnan(local_argmax[y,x]):
-        
-            z1 = int(np.floor(local_argmax[y,x]))
-            z2 = int(np.ceil(local_argmax[y,x]))
-            z1_coeff = np.abs(z2-local_argmax[y,x])
-            z2_coeff = np.abs(z1-local_argmax[y,x])
-        
-            if z1==z2:
-                
-                proj_filt[yi:yi+patch_size,xi:xi+patch_size] = (
-                    img_filt[z1,yi:yi+patch_size,xi:xi+patch_size]
-                    )
-                
-            else:
-                    
-                proj_filt[yi:yi+patch_size,xi:xi+patch_size] = (
-                    img_filt[z1,yi:yi+patch_size,xi:xi+patch_size]*z1_coeff +
-                    img_filt[z2,yi:yi+patch_size,xi:xi+patch_size]*z2_coeff
-                    )
-        
-        else:
-            
-            proj_filt[yi:yi+patch_size,xi:xi+patch_size] = 0
-
-        
-end = time.time()
-print(f'  {(end-start):5.3f} s')
-
-# -----------------------------------------------------------------------------
-
-start = time.time()
-print('??? Proj')
-
-# Get mask
-thresh = threshold_li(proj_filt, tolerance=1)
-mask = proj_filt > thresh*3
-skel = skeletonize(mask)
-
-end = time.time()
-print(f'  {(end-start):5.3f} s')
-
-# -----------------------------------------------------------------------------
-
-# Create filters  
-filters = []    
-thetas = np.arange(0, np.pi, np.pi/n_filters)
-for theta in thetas:        
-    kernel = cv2.getGaborKernel(
-        (kernel_size, kernel_size), 
-        sigma, theta, lmbda, gamma, psi, 
-        ktype=cv2.CV_64F
-        )
-    kernel /= 1.0 * kernel.sum() # Brightness normalization
-    filters.append(kernel)
-
 
 viewer = napari.Viewer()
-# viewer.add_image(np.array(filters))
 viewer.add_image(img)
+# viewer.add_image(rsize)
 # viewer.add_image(mask)
-# viewer.add_image(skel, blending='additive')
-viewer.add_image(proj_filt)
-# viewer.add_image(img_filt)
-# viewer.add_image(local_mean)
-# viewer.add_image(local_argmax)
-# viewer.add_vectors(vectors, length=patch_size//2)
-
-#%%
-
-# io.imsave(
-#     Path(data_path, raw_name.replace('.tif', '_tiles.tif')), 
-#     np.array(tiles), check_contrast=False)
-
-# io.imsave(
-#     Path(data_path, raw_name.replace('.tif', '_filt.tif')), 
-#     img_filt.astype('uint16'), check_contrast=False)
+# viewer.add_image(magnitudes)
+# viewer.add_image(orientations)
+viewer.add_image(gabor)
+viewer.add_image(skel)
+viewer.add_image(labels)
